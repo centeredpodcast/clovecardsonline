e203171a59eb25f45361f8e013fc0aa85d535ca4
Sapphire7x <78709769+Sapphire7x@users.noreply.github.com>
2021-02-22 22:40:38 -0800

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 $("h1").html("Hello!") //This is just filler stuff.
 
-const cards; //This will eventually get all of the cards possible!
+const cards = 0; //This will eventually get all of the cards possible!
 let yourCards = []; //This will eventually be a list of all the cards you have.
 let activeDeck; //Ths will eventually be the ID (if you will) of the deck that you are currently using.
 let decks = {}; //This will eventually be decks that you have.
