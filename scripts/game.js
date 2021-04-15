83e825a99a44d51f7377286dd8ac102419c27a84
Sapphire7x <jstn.xu@gmail.com>
2021-04-14 22:52:52 -0700

    Added comments, optimized the code, and troubleshooted with the delete deck function.

--- scripts/game.js
+++ scripts/game.js
@@ -6,6 +6,6 @@ const cards = {
         "otherData": "lol"
     }
 }; //This will eventually get all of the cards possible and their data!!
-let yourCardsData = ["name", "secondone", "lol", "secondone"]; //This will eventually be a list of all the cards you have.
-let activeDeckData = 0; //Ths will eventually be the ID (if you will) of the deck that you are currently using.
-let decksData = [["name", "secondone"], ["name"]]; //This will eventually be decks that you have.
+let yourCards = ["name", "secondone", "lol", "secondone"]; //This will eventually be a list of all the cards you have.
+let activeDeck = 0; //Ths will eventually be the ID (if you will) of the deck that you are currently using.
+let decks = [["name", "secondone"], ["name"]]; //This will eventually be decks that you have.
